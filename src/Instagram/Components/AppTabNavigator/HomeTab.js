d80ba0ecb6f67de987d1215f8e65fe00b03c304e
Hardik <hdnakarani@gmail.com>
2019-05-03 14:17:11 +0530

    fix header of profile

--- src/Instagram/Components/AppTabNavigator/HomeTab.js
+++ src/Instagram/Components/AppTabNavigator/HomeTab.js
@@ -1,85 +1,163 @@
-import React, {Component} from 'react';
-import {View, StyleSheet, Text, ScrollView} from 'react-native';
+import React, { Component } from "react";
+import { View, StyleSheet, Text, ScrollView, Platform } from "react-native";
 
-import {Icon, Container, Content, Thumbnail} from 'native-base'
+import {
+  Header,
+  Icon,
+  Container,
+  Content,
+  Thumbnail,
+  Left,
+  Right,
+  Body
+} from "native-base";
 
-import CardComponent from '../CardComponent';
+import CardComponent from "../CardComponent";
 
 export default class HomeTab extends Component {
+  static navigationOptions = {
+    tabBarIcon: ({ tintColor }) => (
+      <Icon name="ios-home" style={{ color: tintColor }} />
+    )
+  };
 
+  render() {
+    return (
+      <Container style={styles.container}>
+        <Header style={[styles.androidHeader]}>
+          <Left>
+            <Icon name={"camera"} style={{ padding: 10 }} />
+          </Left>
+          <Body style={[styles.androidHeaderTitle]}>
+            <Text style={{ fontWeight: "bold" }}>Instagram</Text>
+          </Body>
+          <Right>
+            <Icon name={"send"} style={{ padding: 10 }} />
+          </Right>
+        </Header>
+        <Content>
+          <View style={{ height: 100 }}>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                paddingHorizontal: 7
+              }}
+            >
+              <Text style={{ fontWeight: "bold" }}>Stories</Text>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Icon name="md-play" style={{ fontSize: 14 }} />
+                <Text style={{ fontWeight: "bold" }}> Watch All</Text>
+              </View>
+            </View>
+            <View style={{ flex: 3 }}>
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{
+                  alignItems: "center",
+                  paddingStart: 5,
+                  paddingEnd: 5
+                }}
+              >
+                <Thumbnail
+                  style={{
+                    marginHorizontal: 5,
+                    borderColor: "pink",
+                    borderWidth: 2
+                  }}
+                  source={require("../../assets/storiesHeaderThumbnails/1.jpg")}
+                />
+                <Thumbnail
+                  style={{
+                    marginHorizontal: 5,
+                    borderColor: "pink",
+                    borderWidth: 2
+                  }}
+                  source={require("../../assets/storiesHeaderThumbnails/2.jpeg")}
+                />
+                <Thumbnail
+                  style={{
+                    marginHorizontal: 5,
+                    borderColor: "pink",
+                    borderWidth: 2
+                  }}
+                  source={require("../../assets/storiesHeaderThumbnails/3.jpeg")}
+                />
+                <Thumbnail
+                  style={{
+                    marginHorizontal: 5,
+                    borderColor: "pink",
+                    borderWidth: 2
+                  }}
+                  source={require("../../assets/storiesHeaderThumbnails/4.jpeg")}
+                />
+                <Thumbnail
+                  style={{
+                    marginHorizontal: 5,
+                    borderColor: "pink",
+                    borderWidth: 2
+                  }}
+                  source={require("../../assets/storiesHeaderThumbnails/5.jpeg")}
+                />
+                <Thumbnail
+                  style={{
+                    marginHorizontal: 5,
+                    borderColor: "pink",
+                    borderWidth: 2
+                  }}
+                  source={require("../../assets/storiesHeaderThumbnails/6.jpeg")}
+                />
+                <Thumbnail
+                  style={{
+                    marginHorizontal: 5,
+                    borderColor: "pink",
+                    borderWidth: 2
+                  }}
+                  source={require("../../assets/storiesHeaderThumbnails/7.jpeg")}
+                />
+                <Thumbnail
+                  style={{
+                    marginHorizontal: 5,
+                    borderColor: "pink",
+                    borderWidth: 2
+                  }}
+                  source={require("../../assets/storiesHeaderThumbnails/8.jpeg")}
+                />
+              </ScrollView>
+            </View>
+          </View>
 
-    static navigationOptions = {
-        tabBarIcon: ({tintColor}) => (
-            <Icon name="ios-home" style={{color: tintColor}}/>
-        )
-    }
+          <CardComponent imageSource="1" likes="100" />
 
+          <CardComponent imageSource="2" likes="200" />
 
-    render() {
-        return (
-            <Container style={styles.container}>
-                <Content>
-                    <View style={{height: 100}}>
-                        <View style={{
-                            flex: 1,
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            alignItems: 'center',
-                            paddingHorizontal: 7
-                        }}>
-                            <Text style={{fontWeight: 'bold'}}>Stories</Text>
-                            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                                <Icon name='md-play' style={{fontSize: 14}}/>
-                                <Text style={{fontWeight: 'bold'}}> Watch All</Text>
-                            </View>
-
-                        </View>
-                        <View style={{flex: 3}}>
-                            <ScrollView
-                                horizontal={true}
-                                showsHorizontalScrollIndicator={false}
-                                contentContainerStyle={{
-                                    alignItems: 'center',
-                                    paddingStart: 5,
-                                    paddingEnd: 5
-                                }}
-                            >
-                                <Thumbnail style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}}
-                                           source={require('../../assets/storiesHeaderThumbnails/1.jpg')}/>
-                                <Thumbnail style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}}
-                                           source={require('../../assets/storiesHeaderThumbnails/2.jpeg')}/>
-                                <Thumbnail style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}}
-                                           source={require('../../assets/storiesHeaderThumbnails/3.jpeg')}/>
-                                <Thumbnail style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}}
-                                           source={require('../../assets/storiesHeaderThumbnails/4.jpeg')}/>
-                                <Thumbnail style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}}
-                                           source={require('../../assets/storiesHeaderThumbnails/5.jpeg')}/>
-                                <Thumbnail style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}}
-                                           source={require('../../assets/storiesHeaderThumbnails/6.jpeg')}/>
-                                <Thumbnail style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}}
-                                           source={require('../../assets/storiesHeaderThumbnails/7.jpeg')}/>
-                                <Thumbnail style={{marginHorizontal: 5, borderColor: 'pink', borderWidth: 2}}
-                                           source={require('../../assets/storiesHeaderThumbnails/8.jpeg')}/>
-                            </ScrollView>
-
-                        </View>
-                    </View>
-
-                    <CardComponent imageSource="1" likes="100"/>
-
-                    <CardComponent imageSource="2" likes="200"/>
-
-
-                    <CardComponent imageSource="3" likes="300"/>
-                </Content>
-            </Container>
-        )
-    }
+          <CardComponent imageSource="3" likes="300" />
+        </Content>
+      </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white'
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  androidHeader: {
+    ...Platform.select({
+      android: {
+        backgroundColor: "white"
+      }
+    })
+  },
+  androidHeaderTitle: {
+    ...Platform.select({
+      android: {
+        alignItems: "flex-end"
+      }
+    })
+  }
+});
--- src/Instagram/Components/MainScreen.js
+++ src/Instagram/Components/MainScreen.js
@@ -1,63 +1,56 @@
 import React, { Component } from "react";
-import {SafeAreaView, Text, StyleSheet, Platform} from "react-native";
-import {createBottomTabNavigator, createAppContainer} from 'react-navigation'
-import {Icon} from "native-base";
+import { SafeAreaView, Text, StyleSheet, Platform } from "react-native";
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import { Icon } from "native-base";
 
-
-import HomeTab from './AppTabNavigator/HomeTab';
-import AddMediaTab from './AppTabNavigator/AddMediaTab';
-import SearchTab from './AppTabNavigator/SearchTab';
-import LikesTab from './AppTabNavigator/LikesTab';
-import ProfileTab from './AppTabNavigator/ProfileTab';
-
-
+import HomeTab from "./AppTabNavigator/HomeTab";
+import AddMediaTab from "./AppTabNavigator/AddMediaTab";
+import SearchTab from "./AppTabNavigator/SearchTab";
+import LikesTab from "./AppTabNavigator/LikesTab";
+import ProfileTab from "./AppTabNavigator/ProfileTab";
 
 class MainScreen extends Component {
-    static navigationOptions = {
-        headerLeft: <Icon name="camera" style={{paddingLeft: 15}}/>,
-        title: "Instagram",
-        headerRight: <Icon name="send" style={{paddingRight: 15}}/>
-    };
+  static navigationOptions = {
+    header: null
+  };
 
   render() {
-    return (
-
-        <AppTabNavigator/>
-
-    );
+    return <AppTabNavigator />;
   }
 }
 
 export default MainScreen;
 
-const TabsNavigator = createBottomTabNavigator({
+const TabsNavigator = createBottomTabNavigator(
+  {
     HomeTab,
     SearchTab,
     AddMediaTab,
     LikesTab,
     ProfileTab
-}, {
+  },
+  {
     animationEnabled: true,
     swipeEnabled: true,
-    tabBarPosition: 'bottom',
+    tabBarPosition: "bottom",
     tabBarOptions: {
-        style: {
-            ...Platform.select({
-                android: {
-                    backgroundColor: 'white'
-                }
-            })
-        },
+      style: {
+        ...Platform.select({
+          android: {
+            backgroundColor: "white"
+          }
+        })
+      },
 
-        activeTintColor: '#000',
-        inactiveTintColor: '#d1cece',
-        showLabel: false,
-        showIcon: true
+      activeTintColor: "#000",
+      inactiveTintColor: "#d1cece",
+      showLabel: false,
+      showIcon: true
     }
-})
+  }
+);
 
 const AppTabNavigator = createAppContainer(TabsNavigator);
-
 
 const styles = StyleSheet.create({
   container: {
